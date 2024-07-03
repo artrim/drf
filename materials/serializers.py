2f5f033889372ec07d8e6e38f4b1b36fb3a2a85f
Артур Хамидуллин <artrimskiy@gmail.com>
2024-07-03 22:49:31 +0500

    create SubscriptionSerializer and get_subscription

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.validators import url_validator
 
 
@@ -12,9 +12,24 @@ class LessonSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class SubscriptionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    subscription = serializers.SerializerMethodField()
+
+    def get_subscription(self, obj):
+        request = self.context.get('request')
+        user = None
+        if request:
+            user = request.user
+        return obj.subscription_set.filter(user=user).exists()
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
